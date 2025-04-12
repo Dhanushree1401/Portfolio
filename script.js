c38affddbe6e442f6c88d8b9ad6b9179423704ce
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2025-04-12 21:54:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Full Stack developer','AR/VR Enthusiast','Game Developer'],
+    strings:['Full Stack Developer','AR/VR Enthusiast','Game Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
